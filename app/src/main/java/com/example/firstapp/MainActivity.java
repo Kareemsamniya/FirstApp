5d85a9407c8b1515670fe74f951488d7a3a6109a
MahmodAli25 <mahmod.alia620@gmail.com>
2023-09-14 14:59:49 +0300

    to do

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("","");
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ka ","onRestart");
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("k ","onResume");
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("k ","onResume");
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
     }
 }
