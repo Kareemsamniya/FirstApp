a3cd5c034c95946b22684e9904462cef0b453eef
MahmodAli25 <mahmod.alia620@gmail.com>
2023-10-01 09:41:05 +0300

    to do

--- app/src/main/java/com/example/firstapp/SignInActivity.java
+++ app/src/main/java/com/example/firstapp/SignInActivity.java
@@ -3,12 +3,17 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private TextInputLayout etEmail;
+    private TextInputLayout etpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
     }
 }
