1b40edf70a508e95f0c4f5c1baedb2a0c841876b
MahmodAli25 <mahmod.alia620@gmail.com>
2023-10-02 13:55:11 +0300

    to do

--- app/src/main/java/com/example/firstapp/SignInActivity.java
+++ app/src/main/java/com/example/firstapp/SignInActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
 
     private TextInputLayout etEmail;
     private TextInputLayout etpassword;
+    private TextInputLayout btnsignin;
+    private TextInputLayout btnsignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        etEmail= (TextInputLayout) findViewById(R.id.etEmail);
+        etpassword= (TextInputLayout) findViewById(R.id.etpassword);
+        Button btnsignin= (Button) findViewById(R.id.btnsignin);
+        Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
 
     }
 }
