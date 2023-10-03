59b26927aae785eb6d6c8ba60a3a6e9611e4b1ee
MahmodAli25 <mahmod.alia620@gmail.com>
2023-10-03 12:15:03 +0300

    to do

--- app/src/main/java/com/example/firstapp/data/AppDatabase.java
+++ app/src/main/java/com/example/firstapp/data/AppDatabase.java
@@ -1,4 +1,31 @@
 package com.example.firstapp.data;
 
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.Database;
+
+@Database(entities = {MyUser.class, MySubject.class, MyTask.class},version = 5)
+
 public class AppDatabase {
+    private static AppDatabase db;
+
+    public abstract MyUserQuery getMyUserQuery();
+
+    public abstract MySubjectQuery getMySubjectQuery();
+
+    public abstract MyTaskQuery getMyTaskQuery();
+
+    public static AppDatabase getDB(Context context) {
+        if (db == null) {
+            db = Room.databaseBuilder(context,
+                            AppDatabase.class, "database-name")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return db;
+
+    }
 }
