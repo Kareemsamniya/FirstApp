30f243093d78803c6811dfb4193abaf42b93d8cd
MahmodAli25 <mahmod.alia620@gmail.com>
2023-10-04 10:48:22 +0300

    to do

--- app/src/main/java/com/example/firstapp/data/AppDatabase.java
+++ app/src/main/java/com/example/firstapp/data/AppDatabase.java
@@ -12,21 +12,54 @@ import com.example.firstapp.data.myTasksTable.MyTaskQuery;
 import com.example.firstapp.data.useresTable.MyUser;
 import com.example.firstapp.data.useresTable.MyUserQuery;
 
-@Database(entities = {MyUser.class, MySubject.class, MyTask.class},version = 5)
-
-public abstract class AppDatabase extends RoomDatabase {
+/*
+تعريف الجداول ورقم النسخة
+version
+عند احداث تغيير يخص جدول او جداول علينا تغيير رقم الأصدار ليتم بناء قاعدة البينات من جديد
+ */
+@Database(entities = {MyUser.class, MySubject.class, MyTask.class},version = 1)
+/**
+ * الفئة المسئولة عن بناء قاعدة البيانات لكل جداولها
+ * وتوفر لنا كائن للتعامل مع قاعدة البيانات
+ */
+public abstract class AppDatabase extends RoomDatabase
+{
+    /**
+     * كائن للتعامل مع قاعدة البيانات
+     */
     private static AppDatabase db;
+
+    /**
+     * يعيد كائن لعمليات جدول المستعملين
+     * @return
+     */
 
     public abstract MyUserQuery getMyUserQuery();
 
+    /**
+     * يعيد كائن لعمليات جدول المواضيع
+     * @return
+     */
+
     public abstract MySubjectQuery getMySubjectQuery();
 
+    /**
+     * يعيد كائن لعمليات جدول المهمات
+     * @return
+     */
+
     public abstract MyTaskQuery getMyTaskQuery();
+
+    /**
+     * بناء قاعدة البيانات واعادة كائن يؤشر عليها
+     * @param context
+     * @return
+     */
 
     public static AppDatabase getDB(Context context) {
         if (db == null) {
             db = Room.databaseBuilder(context,
-                            AppDatabase.class, "database-name")
+                            AppDatabase.class, "database-name")//اسم
                     .fallbackToDestructiveMigration()
                     .allowMainThreadQueries()
                     .build();
