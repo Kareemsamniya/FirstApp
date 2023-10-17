f7c06af4062bc8a8438985d9cc4573bfaf07516d
kareem_home <kareem.samniya@gmail.com>
2023-10-17 13:33:18 +0300

    to do

--- app/src/main/java/com/example/firstapp/SignUpActivity.java
+++ app/src/main/java/com/example/firstapp/SignUpActivity.java
@@ -3,12 +3,31 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextInputLayout etEmail;
+    private TextInputLayout etpassword;
+    private TextInputLayout etRepassword;
+    private TextInputLayout etname;
+    private TextInputLayout etphone;
+    private Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        etEmail= (TextInputLayout) findViewById(R.id.etEmail);
+        etpassword= (TextInputLayout) findViewById(R.id.etpassword);
+        etRepassword= (TextInputLayout) findViewById(R.id.etRepassword);
+        etname= (TextInputLayout) findViewById(R.id.etname);
+        etphone= (TextInputLayout) findViewById(R.id.etphone);
+        btnSave=(Button) findViewById(R.id.btnSave);
+
+
     }
 }
