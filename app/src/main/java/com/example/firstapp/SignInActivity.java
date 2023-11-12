3679ee25e2f095d5fb09fdd1ac51b28c1ab3bef8
MahmodAli25 <mahmod.alia620@gmail.com>
2023-11-12 09:41:02 +0200

    to do

--- app/src/main/java/com/example/firstapp/SignInActivity.java
+++ app/src/main/java/com/example/firstapp/SignInActivity.java
@@ -15,8 +15,7 @@ public class SignInActivity extends AppCompatActivity {
     private TextInputEditText etSignInPassword;
     private Button btnSignInSignIn;
     private Button btnSignInSignUp;
-    private Button btnSignInSave;
-    private Button btnSignInCancel;
+
 
 
     @Override
@@ -27,8 +26,7 @@ public class SignInActivity extends AppCompatActivity {
         etSignInPassword=  findViewById(R.id.etSignInPassword);
         btnSignInSignIn=  findViewById(R.id.btnSignInSignIn);
         btnSignInSignUp= findViewById(R.id.btnSignInSignUp);
-        btnSignInSave= findViewById(R.id.btnSignInSave);
-        btnSignInCancel= findViewById(R.id.btnSignInCancel);
+
 
 
     }
@@ -37,6 +35,6 @@ public class SignInActivity extends AppCompatActivity {
 
         Intent i = new Intent(SignInActivity.this, SignUpActivity.class);
         startActivity(i);
-        finish();
+
     }
 }
