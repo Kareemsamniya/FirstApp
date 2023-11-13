589e2facded984322099e07913cbb2108899dbfd
MahmodAli25 <mahmod.alia620@gmail.com>
2023-11-13 14:11:55 +0200

    to do

--- app/src/main/java/com/example/firstapp/SignInActivity.java
+++ app/src/main/java/com/example/firstapp/SignInActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -37,6 +38,11 @@ public class SignInActivity extends AppCompatActivity {
         startActivity(i);
 
     }
+    public void onClickSignInSignIn(View V)
+    {
+
+        checkEmailPassw();
+    }
     private void checkEmailPassw()
     {
         boolean isAllOk = true;// يحوي نتيجة فحص الحقول ان كانت سليمة
@@ -45,9 +51,24 @@ public class SignInActivity extends AppCompatActivity {
         //استخراج نص كلمة المرور
         String password = etSignInPassword.getText().toString();
         //فحص الايميل ان كان طوله اقل من 6 او لا يحوي @ فهو خطأ
-        if (etSignInEmail.length() < 6)
+        if (etSignInEmail.length() < 6 || email.contains("@") == false)
         {
-
+            //تعديل المتغير ليدل على ان الفحص يعطي نتيجة خاطئة
+            isAllOk = false;
+            //عرض ملاحظة خطأ على الشاشة داخل حقل البريد
+            etSignInPassword.setError("Wrong Password");
         }
+        if(etSignInPassword.length() < 8 || password.contains("") == true)
+        {
+            isAllOk = false;
+            etSignInPassword.setError("Wrong Password");
+        }
+        if(isAllOk)
+        {
+            Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
+
 }
