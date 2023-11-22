87a59c637b51da85a98fb10ae616fe0dc3083e87
MahmodAli25 <mahmod.alia620@gmail.com>
2023-11-22 14:12:15 +0200

    to do

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -45,44 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        //spnr2
-        spnrMainSubject=findViewById(R.id.spnrMainSubject);
-        //spnr3 prepare adapter
-        ArrayAdapter<String>adapter=new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //spnr 4 data source
-        String[] ar={"Math","CS","Phs","Arb","Eng"};
-        //spnr 5
-        adapter.addAll(ar);
-        //spnr 6
-        spnrMainSubject.setAdapter(adapter);
-
-
-        Log.d("","");
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-
-        //بناء قاعدة بيانات وأرجاع مؤشر عليها(1
-        AppDatabase db=AppDatabase.getDB(getApplicationContext());
-        //مؤشر لجدول(2
-        MySubjectQuery subjectQuery = db.getMySubjectQuery();
-        // 3) بناء كائن من نوع الجدول وتحديد قيم الصفات
-        MySubject s1=new MySubject();
-        s1.Title="math";
-        MySubject s2= new MySubject();
-        s2.Title="computers";
-        //4) اضافة كائن للجدول
-        subjectQuery.insert(s1);
-        subjectQuery.insert(s2);
-
-        //5) فحص هل تم حفظ ما سبق
-        //استخراج وطباعة جميع معطيات جدول المواضيع
-        final List<MySubject> allSubjects = subjectQuery.getAll();
-        for(MySubject s : allSubjects)
+        /**
+         * عملية تجهيز السبنر بالمواضيع
+         */
+        private void initSubjectSpnr()
         {
-            Log.d("kareem",s.Title);
-            Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+            AppDatabase db = AppDatabase.getDB(getApplicationContext());
+            MySubjectQuery subjectQuery = db.getMySubjectQuery();
+            List<MySubject>allSubjects = subjectQuery.getAll();
         }
+
 
 
     }
