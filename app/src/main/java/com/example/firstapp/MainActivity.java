b91e996d7d5fca29a15605a59f173db916d0db7d
MahmodAli25 <mahmod.alia620@gmail.com>
2023-11-26 12:21:26 +0200

    to do

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,7 +13,6 @@ import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.firstapp.data.AppDatabase;
@@ -136,10 +136,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override//بناء القائمة
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.main_menu, menu);
-return true;
+        return true;
+    }
+    @Override//معالجة حدث اختيار عنصر من القائمة
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId()==R.id.itemSettings)
+        {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId()==R.id.itemSignOut)
+        {
+            Toast.makeText(this, "Log Out", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId()==R.id.itemAddTask)
+        {
+        }
+        return true;
     }
 
 
